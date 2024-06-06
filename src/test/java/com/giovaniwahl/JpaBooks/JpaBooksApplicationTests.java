24d93c11f61e1b6934f7f53f3412f658dc703905
Giovani Wahl <giovaniwahl@gmail.com>
2024-06-06 12:49:59 -0300

    inicio projeto

---
+++ src/test/java/com/giovaniwahl/JpaBooks/JpaBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovaniwahl.JpaBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
